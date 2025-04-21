400642b5f1e12a88438989dca8215ba0d6fd27e2
adhil2399 <adhi23996@gmail.com>
2025-04-21 09:18:54 +0530

    final commit

--- backend/src/controllers/user/UpdateProfile.ts
+++ backend/src/controllers/user/UpdateProfile.ts
@@ -4,15 +4,23 @@ import StatusCode from "../../config/statusCode";
 
 export const updateprofile = async(req:Request,res:Response)=>{
     try {
-        
        const user = req.body
-       console.log(user)
-       console.log(user._id)
+    //    console.log(user)
+    //    console.log(user._id)
+
+     const existEmail = await User.findOne({email:user.email})
+        // console.log(user)
+        // console.log(existEmail)
+     if (existEmail && (existEmail._id as string).toString() !== user._id) {
+        res.status(StatusCode.CONFLICT).json({message:'this email already used'})
+        return
+     }
 
     const updatedUser = await User.findByIdAndUpdate(user._id,{$set:{...user}},{new:true})
     //  console.log(updatedUser)
 
      res.status(StatusCode.OK).json({message: 'profile updated',updatedUser})
+     return
     } catch (error) {
         if (error instanceof Error) {
             console.log("registerUser ERROR :", error);
--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,16 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { JwtPayload, verify } from "jsonwebtoken";
+import StatusCode from "../config/statusCode";
 
 export const tokenChecker = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers["x-access-token"] as string;
   if (!token) {
-    res.status(401).json({ error: "Access token missing" });
+    res.status(StatusCode.UNAUTHORIZED).json({ error: "Access token missing" });
     return 
   }
 
   try {
     const decoded = verify(token, process.env.JWT_SECRET as string) as JwtPayload;
-    req.user = decoded;
+    req.user = decoded.id;
 
     console.log("Authorized:", decoded);
     next();
